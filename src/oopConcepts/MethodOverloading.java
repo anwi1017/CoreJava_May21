23363437d9c0124b4b2919baa4ef857450f0f913
Ann Wilson <anwi1017@gmail.com>
2021-05-15 04:08:13 +0000

    method overloading lesson

---
+++ src/oopConcepts/MethodOverloading.java
@@ -0,0 +1,19 @@
+package oopConcepts;
+
+public class MethodOverloading {
+
+	public void add(int a, int b) {
+		
+		int sum = a+b;
+		
+		System.out.println("Int output " + sum);
+	}
+	
+	public void add(double a, double b) {
+		
+		double sum = a+b;
+		
+		System.out.println("Double output " + sum);
+	}
+
+}
--- src/oopConcepts/RunnerClass.java
+++ src/oopConcepts/RunnerClass.java
@@ -5,12 +5,12 @@ public class RunnerClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		ClassA Emp2 = new ClassA();
+		/*ClassA Emp2 = new ClassA();
 		ClassA Emp1 = new ClassA(10000,"Ann","XYZ");
 		/*
 		Emp1.Name = "Ann";
 		Emp1.OrgName = "XYZ";
-		Emp1.Salary = 10000;*/
+		Emp1.Salary = 10000;
 				
 		Emp1.Display();
 		
@@ -25,7 +25,13 @@ public class RunnerClass {
 		
 		ClassC Obj2 = new ClassC();
 		
-		Obj2.run();
+		Obj2.run();*/
+		
+		MethodOverloading Obj3 = new MethodOverloading();
+		
+		Obj3.add(5, 7);
+		Obj3.add(5.5, 7.7);
+		
 	}
 
 }
